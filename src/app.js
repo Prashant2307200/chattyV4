0fb0f1a7e58d62c19a997bd39d098550a3fc0561
user.name like: "Prashant Dobariya <user.email like: "prashantdobariya276@gmail.com>
2025-05-23 15:52:22 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ if (NODE_ENV === "production") {
   app.use(helmet(helmetConfig));
   app.use(rateLimit(rateLimitConfig));
 
-  app.use(express.static(path.resolve(path.dirname(process.execPath), "client", "dist"), {
+  app.use(express.static(path.resolve(path.dirname(process.execPath), "../package-lock.jsonclient", "dist"), {
     maxAge: '1y',
     setHeaders: (res, filePath) => {
       if (filePath.endsWith('index.html'))
@@ -58,7 +58,7 @@ if (NODE_ENV === "production") {
   }));
 
   app.get('/', (_request, response) => {
-    return response.sendFile(path.resolve(path.dirname(process.execPath), "client", "dist", "index.html"));
+    return response.sendFile(path.resolve(path.dirname(process.execPath), "../client", "dist", "index.html"));
   });
 } else {
 
