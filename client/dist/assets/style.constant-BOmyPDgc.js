f91f7e6819b1fbd64225cfddf3477af5a7d59d9b
user.name like: "Prashant Dobariya <user.email like: "prashantdobariya276@gmail.com>
2025-05-24 18:39:24 +0530

    fix

--- client/dist/assets/style.constant-BOmyPDgc.js
+++ client/dist/assets/style.constant-BOmyPDgc.js
@@ -1,2 +1,1 @@
 const t={title:"text-2xl font-bold",subtitle:"text-base-content/60",base:"bg-base-200",primary:"bg-primary/10",primaryHover:"bg-primary/20"};export{t as s};
-//# sourceMappingURL=style.constant-BOmyPDgc.js.map
