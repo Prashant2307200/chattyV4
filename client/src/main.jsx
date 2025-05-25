5f4754d48f99fbb3b1640a664b06d1d1d4f02f21
user.name like: "Prashant Dobariya <user.email like: "prashantdobariya276@gmail.com>
2025-05-25 09:30:33 +0530

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { registerSW } from 'virtual:pwa-register';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { registerSW } from 'virtual:pwa-register'; 
 
 import './index.css';
 import App from './App.jsx';
@@ -13,9 +12,7 @@ import { TanStackQueryProvider } from './providers/TanstackQueryProvider.jsx';
 const updateSW = registerSW({
   onNeedRefresh() {},
   onOfflineReady() {}
-});
-
-serviceWorkerRegistration.register();
+}); 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
